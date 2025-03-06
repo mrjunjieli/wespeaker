5edb7090e5c73e6039d50b463af425adf9c200ea
mrjunjieli <mrjunjieli@qq.com>
2025-03-06 18:14:46 +0800

    kld_loss

--- wespeaker/models/speaker_model.py
+++ wespeaker/models/speaker_model.py
@@ -24,6 +24,8 @@ import wespeaker.models.whisper_PMFA as whisper_PMFA
 import wespeaker.models.redimnet as redimnet
 import wespeaker.models.samresnet as samresnet
 import wespeaker.models.xi_vector as xi_vector
+import wespeaker.models.xi_vector_plus_VAE as xi_vector_plus_vae
+
 
 
 
@@ -52,6 +54,8 @@ def get_speaker_model(model_name: str):
         return getattr(samresnet, model_name)
     elif model_name.startswith("XI_VEC"):
         return getattr(xi_vector, model_name)
+    elif model_name.startswith("VAE_XI_VEC"):
+        return getattr(xi_vector_plus_vae, model_name)
     else:  # model_name error !!!
         print(model_name + " not found !!!")
         exit(1)
